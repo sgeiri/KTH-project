18d38dba64ca156221c084606ca3c99aad8450d1
Sigurgeir <sigun@tdcnet.dk>
2022-05-03 19:24:10 +0200

    Working simulation, missing tuning

--- code/simulation.py
+++ code/simulation.py
@@ -2,83 +2,102 @@
 
 import simpy
 import itertools
+import pandas as pd
 from numpy import random
 
-
-RANDOM_SEED = 42
-RADIO_SIZE = 2
-
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# ENV variables
+# NUM_UE is the number of devices in the simulation environment
 NUM_UE = 1000
 
-TX_BYTES = [0, 100]
-T_INTER = [30, 300]        # Create a car every [min, max] seconds
-SIM_TIME = 1000            # Simulation time in seconds
+# SIM_TIME the number of iteration over the simulation
+# one unit is one millisecond i.e. one TTI - not yet
+SIM_TIME = 10000
+
+# AVG_IDLE is the average time between transmissions
+# It is set to 5 minuttes or 300.000 ms
+AVG_IDLE = 300
+
+# AVG_BYTES is the average number of bytes transmitted per session
+AVG_BYTES = 4e3
+
+# AVG_TX is the average number of bytes transmitted per TTI
+TX_AVG = 1000
+TX_VAR = 20
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# MEAS VARIABLES
+progRes = pd.DataFrame(columns=['ts','ue','txBytes','remainBytes','state'])
+
+# UE's generated unconnected with exponentially distributed waiting time
+# the average waiting time is 1 hour i.e. the device sends every hour some data
+# NOTE: one constraint could be minimum 1 hour between transmissions
+ueBase = { i :{'state':'idle', 'nextTx':round(random.exponential(AVG_IDLE)), 'txBytes':0} for i in range(NUM_UE) }
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# ue trying to connect to the RACH, it is here the collision
+# is simulated, catched and registered
+def ueRach(env, ueBase, progRes):
+  rachAvail = 0
+
+  # Pulling out the ue's trying to connect
+  kList = [k for k, v in ueBase.items() if v['nextTx']==env.now and v['state']=='idle']
+  random.shuffle(kList)
+
+  for k in kList:
+    if (rachAvail==0):
+      rachAvail = 1
+      ueBase[k]['state'] = 'rach'
+      progRes.loc[len(progRes)] = [env.now, k, None, None, 'rach']
+    else:
+      progRes.loc[len(progRes)] = [env.now, k, None, None, 'collision']
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-def simEnv(env):
-  while True:
-    for ue in range(NUM_UE):
-      if (random.uniform(1000)<100):
-        print(str(env.now)+":", ue, "transmitted !")
+# ue connected to the RACH in last run, changing state and setting up
+# the session parameters
+def ueCon(env, ueBase, progRes):
+  kList = [k for k, v in ueBase.items() if v['state']=='rach']
 
-    # One step at a time
+  for k in kList:
+    ueBase[k]['state'] = 'connected'
+    ueBase[k]['txBytes'] = round(random.exponential(AVG_BYTES))
+    progRes.loc[len(progRes)] = [env.now, k, None, ueBase[k]['txBytes'], 'connected']
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# here the txBytes are transmitted and reduced until all are sent
+# and the ue can change to idle state
+def ueTx(env, ueBase, progRes):
+  kList = [k for k, v in ueBase.items() if v['state']=='connected']
+
+  for k in kList:
+    tx = max(0, round(random.normal(TX_AVG, TX_AVG)))
+    ueBase[k]['txBytes'] = max(0, ueBase[k]['txBytes']-tx)
+    progRes.loc[len(progRes)] = [env.now, k, tx, ueBase[k]['txBytes'], 'connected']
+    
+    if (ueBase[k]['txBytes']==0):
+      ueBase[k]['state'] = 'idle'
+      ueBase[k]['nextTx'] = round(random.exponential(AVG_IDLE))
+      progRes.loc[len(progRes)] = [env.now, k, None, None, 'idle']
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# SIMULATION environment
+def simEnv(env, ueBase, progRes):
+  while True:
+    ueCon(env, ueBase, progRes)
+    ueRach(env, ueBase, progRes)
+
+    ueTx(env, ueBase, progRes)
+
     yield env.timeout(1)
 
-
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 env = simpy.Environment()
-# rach = simpy.Resource(env, 1)
 
-env.process(simEnv(env))
-env.run(until=200)
-
-"""
-def ue(name, env, base_station, radio_res):
-  tx_bytes = random.randint(*TX_BYTES)
-  
-  with base_station.request() as req:
-    start = env.now
-    # Request one of the gas pumps
-    yield req
-
-    yield radio_res.get()
-
-    # The "actual" refueling process takes some time
-    yield env.timeout(bytes_sent / REFUELING_SPEED)
-
-    print('%s finished refueling in %.1f seconds.' % (name, env.now-start))
-
-
-def base_station_control(env, radio_res):
-
-  while True:
-    if radio_res.level / radio_res.capacity * 100 < THRESHOLD:
-      # We need to call the tank truck now!
-      print('Calling tank truck at %d' % env.now)
-      # Wait for the tank truck to arrive and refuel the station
-      yield env.process(tank_truck(env, radio_res))
-
-    yield env.timeout(10)  # Check every 10 seconds
-
-
-def ue_gen(env, base_station):
-  for i in itertools.count():
-    yield env.timeout(random.randint(*T_INTER))
-    env.process(ue('UE %d' % i, env, base_station))
-
-
-# Setup and start the simulation
-print('Gas Station refuelling')
-random.seed(RANDOM_SEED)
-
-# Create environment and start processes
-env = simpy.Environment()
-base_station = simpy.Resource(env, 2)
-radio_res = simpy.Container(env, RADIO_SIZE)
-env.process(base_station_control(env, radio_res))
-env.process(ue_gen(env, base_station, radio_res))
-
-# Execute!
+env.process(simEnv(env, ueBase, progRes))
 env.run(until=SIM_TIME)
-"""
+
